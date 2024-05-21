69c45ed4fb50f23a67460f8bb3896a1ad92af191
Tejas Rajesh <P2746587@my365.dmu.ac.uk>
2024-05-21 17:53:43 +0100

    Adding tstStock.cs

--- AdminSystem/StockList.aspx
+++ AdminSystem/StockList.aspx
@@ -1,4 +1,4 @@
-﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="5List.aspx.cs" Inherits="_1_List" %>
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="StockList.aspx.cs" Inherits="_1_List" %>
 
 <!DOCTYPE html>
 
---
+++ Testing5/tstStock.cs
@@ -0,0 +1,32 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Security.Policy;
+
+namespace Testing5
+{
+    [TestClass]
+    public class tstStock
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //create an instance of the class we want to create
+            clsStock AStock = new clsStock();
+            //test to see that it exists
+            Assert.IsNotNull(AStock);
+        }
+        [TestMethod]
+        public void CurrentStockOK()
+        {
+            //create an instance of the class we want to create
+            clsStock AStock = new clsStock();
+            //create some test data to assign to the property
+            Boolean TestData = true;
+            //assign the data to the property 
+            AStock.CurrentStockLevel = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AStock.CurrentStockLevel, TestData);
+        }
+    }
+}
