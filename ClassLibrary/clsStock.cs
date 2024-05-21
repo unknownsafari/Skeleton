86da6bd4ad53754112c199177914882485b523aa
Tejas Rajesh <P2746587@my365.dmu.ac.uk>
2024-05-21 19:02:01 +0100

    Test Components Addition

--- ClassLibrary/clsStock.cs
+++ ClassLibrary/clsStock.cs
@@ -2,6 +2,12 @@
 {
     public class clsStock
     {
-        public bool CurrentStockLevel { get; set; }
+        public int CurrentStockLevel { get; set; }
+        public int ProductId { get; set; }
+        public string Publisher { get; set; }
+        public string Platform { get; set; }
+        public string ProductName { get; set; }
+        public int MinimumRecorderLEvel { get; set; }
+        public decimal UnitPrice { get; set; }
     }
 }
