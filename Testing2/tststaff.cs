ecbcbd4e743da3937614a334913d18a2da7a8af6
Ding Ma <P2691237@my365.dmu.ac.uk>
2024-05-29 12:49:38 +0100

    29/05

--- Testing2/tststaff.cs
+++ Testing2/tststaff.cs
@@ -10,10 +10,33 @@ namespace Testing2
         [TestMethod]
         public void InstanceOK()
         {
+            //create an instance of the class we want to creat
             clsStaff AnStaff = new clsStaff();
-
+            //test to see that is exists
             Assert.IsNotNull(AnStaff);
-
+        }
+        [TestMethod]
+        public void ActiveProretyOK()
+        { //create an instance of the class we want to create
+            clsStaff AnStaff = new clsStaff();
+            //create some test data to assign to the property
+            Boolean TestData = true;
+            //assign the data to the property
+            AnStaff.Active = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnStaff.Active, TestData);
+        }
+        [TestMethod]
+        public void DateAddedPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsStaff AnAddress = new clsStaff();
+            //create some test data to assign to the property
+            DateTime TestData = DateTime.Now.Date;
+            //assign the data to the property
+            AnAddress.DateAdded = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnAddress.DateAdded, TestData);
         }
     }
 }
